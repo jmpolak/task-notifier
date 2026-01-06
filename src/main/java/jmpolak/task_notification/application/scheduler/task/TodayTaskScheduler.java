b284a3a6b4cedd6b86d42127acd971ba94f722c1
Jean Michel Polak <67679954+jmpolak@users.noreply.github.com>
2026-01-06 11:15:52 +0100

    Issue#1: Schedule task events (#3)
    * Set Berlin TZ & add events and scheduler for mail sending
    
    * Add mail template
    
    * ending

---
+++ src/main/java/jmpolak/task_notification/application/scheduler/task/TodayTaskScheduler.java
@@ -0,0 +1,57 @@
+package jmpolak.task_notification.application.scheduler.task;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.ScheduledFuture;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Service;
+import jmpolak.task_notification.application.event.ScheduleTaskForTodayEvent;
+import jmpolak.task_notification.application.service.MailService;
+import jmpolak.task_notification.core.entity.Task;
+
+@Service
+public class TodayTaskScheduler implements ApplicationListener<ScheduleTaskForTodayEvent> {
+
+    private final TaskScheduler scheduler;
+    private final MailService mailService;
+
+    public TodayTaskScheduler(TaskScheduler scheduler, MailService mailService) {
+        this.scheduler = scheduler;
+        this.mailService = mailService;
+    }
+
+    @Override
+    public void onApplicationEvent(ScheduleTaskForTodayEvent event) {
+        Task task = event.getTask();
+
+        // Ensure task is for today
+        if (!task.getNotificationDate().toLocalDate().equals(LocalDate.now())) {
+            return; // ignore future tasks
+        }
+
+        // Ignore past tasks
+        if (task.getNotificationDate().isBefore(LocalDateTime.now())) {
+            return;
+        }
+
+        Instant instant = task.getNotificationDate().atZone(ZoneId.systemDefault()).toInstant();
+        ScheduledFuture<?> scheduledTask = scheduler.schedule(() -> this.executeTask(task), instant);
+        // scheduledTask.cancel(false); // we can use in memomry object here like map to
+        // store those object
+        // on startup it will populated in a new empty hash map
+        // so updates or delete will be recognized
+        // OR
+        // store it in redis
+        System.out.println("Scheduled todays task " + task.getId() + " at " + instant);
+    }
+
+    private void executeTask(Task task) {
+        System.out.println("Executing todays task: " + task.getTitle());
+        // Your notification logic here
+        this.mailService.sendMail(this.mailService.createMailForTask(task));
+    }
+}
